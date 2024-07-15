1c73c97d2d1eccbf4219cdc83b27a0d715ff0eaa
HsinYi <11050034@me.mcu.edu.tw>
2024-07-15 21:09:44 +0800

    feat(route): 路由設定

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ClearHomeComponent } from './clear-home/clear-home.component';
+import { TemiClassComponent } from './temi-class/temi-class.component';
+import { WebAppClassComponent } from './web-app-class/web-app-class.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'temi' },
+  { path: 'clear-home', component: ClearHomeComponent },
+  { path: 'web-app', component: WebAppClassComponent },
+  { path: 'temi-class', component: TemiClassComponent },
+];
