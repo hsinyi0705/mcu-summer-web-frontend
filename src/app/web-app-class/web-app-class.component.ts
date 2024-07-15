5ded32976ce56ab0d88f34d582479b6d7fa4de33
HsinYi <11050034@me.mcu.edu.tw>
2024-07-15 21:13:10 +0800

    feat(web-app): 建立 web-app 元件

---
+++ src/app/web-app-class/web-app-class.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-app-class',
+  standalone: true,
+  imports: [],
+  templateUrl: './web-app-class.component.html',
+  styleUrl: './web-app-class.component.css'
+})
+export class WebAppClassComponent {
+
+}
