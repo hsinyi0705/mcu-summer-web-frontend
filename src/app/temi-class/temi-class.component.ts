2799bcf2853917821fb63876970b06492b500de8
HsinYi <11050034@me.mcu.edu.tw>
2024-07-15 21:14:00 +0800

    feat(temi-class): 建立 temi-class 元件

---
+++ src/app/temi-class/temi-class.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temi-class',
+  standalone: true,
+  imports: [],
+  templateUrl: './temi-class.component.html',
+  styleUrl: './temi-class.component.css'
+})
+export class TemiClassComponent {
+
+}
