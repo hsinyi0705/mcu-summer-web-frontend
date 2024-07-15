3fd3f9e1e01f17e108d249d2a6fc46d752fce827
HsinYi <11050034@me.mcu.edu.tw>
2024-07-15 21:11:50 +0800

    feat(clear-home): 建立空白首頁元件

---
+++ src/app/clear-home/clear-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clear-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './clear-home.component.html',
+  styleUrl: './clear-home.component.css'
+})
+export class ClearHomeComponent {
+
+}
