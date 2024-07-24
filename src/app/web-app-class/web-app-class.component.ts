b5702f22fdfb83e2579a6c78aa13aab139a84476
HsinYi <11050034@me.mcu.edu.tw>
2024-07-24 19:08:23 +0800

    feat(web-app): 頁面實作

--- src/app/web-app-class/web-app-class.component.ts
+++ src/app/web-app-class/web-app-class.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-web-app-class',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './web-app-class.component.html',
-  styleUrl: './web-app-class.component.css'
+  styleUrls: ['./web-app-class.component.css'],
 })
 export class WebAppClassComponent {
+  form = new FormGroup({
+    id: new FormControl(''),
+    ip: new FormControl(''),
+    locationName: new FormControl(''),
+    goToLocation: new FormControl(''),
+    rotateAngle: new FormControl(''),
+  });
 
+  selectedFunc: string = '';
+
+  selectFunc(functionType: string) {
+    this.selectedFunc = functionType;
+  }
 }
