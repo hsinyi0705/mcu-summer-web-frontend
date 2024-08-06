d9aa4b3f2fcb129bd004e4a590b8a69be57c1f98
HsinYi <11050034@me.mcu.edu.tw>
2024-08-06 10:07:12 +0800

    feat(json): 資料傳入 temi.json 檔案

--- src/app/web-app-class/web-app-class.component.ts
+++ src/app/web-app-class/web-app-class.component.ts
@@ -1,3 +1,103 @@
+// import { CommonModule, NgFor } from '@angular/common';
+// import { HttpClient } from '@angular/common/http';
+// import { Component, OnInit } from '@angular/core';
+// import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+// import { WebSocketService } from '../websocket.service';
+
+// @Component({
+//   selector: 'app-web-app-class',
+//   standalone: true,
+//   imports: [CommonModule, ReactiveFormsModule, NgFor],
+//   templateUrl: './web-app-class.component.html',
+//   styleUrls: ['./web-app-class.component.css'],
+// })
+// export class WebAppClassComponent implements OnInit {
+//   form = new FormGroup({
+//     id: new FormControl(''),
+//     ip: new FormControl(''),
+//     addLocation: new FormControl(''),
+//     goToLocation: new FormControl(''),
+//     deleteLocation: new FormControl(''),
+//     speak: new FormControl(''),
+//   });
+
+//   selectedFunc: string = '';
+//   locations: string[] = []; // 新增一个数组用于存储地点
+
+//   constructor(private webSocketService: WebSocketService, private http: HttpClient) {
+//     // this.webSocketService.message$.subscribe((message) => {
+//     //   console.log('Received message:', message);
+//     //   if (message) {
+//     //     if (message.type === 'onConnect') {
+//     //       console.log('Updating form with ID and IP:', message.id, message.ip);
+//     //       this.form.patchValue({
+//     //         id: message.id,
+//     //         ip: message.ip,
+//     //       });
+//     //     }
+//     //     if (message.command === 'addLocation' && message.status === 'added') {
+//     //       this.locations.push(message.location);
+//     //     } else if (message.command === 'deleteLocation' && message.status === 'deleted') {
+//     //       this.locations = this.locations.filter((loc) => loc !== message.location);
+//     //     }
+//     //   }
+//     // });
+//     this.webSocketService.message$.subscribe((message) => {
+//       console.log('Received message:', message);
+//       if (message) {
+//         if (message.type === 'onConnect') {
+//           console.log('Updating form with ID and IP:', message.id, message.ip);
+//           this.form.patchValue({
+//             id: message.id,
+//             ip: message.ip,
+//           });
+//         }
+//         if (message.command === 'addLocation' && message.status === 'added') {
+//           this.locations.push(message.location);
+//         } else if (message.command === 'deleteLocation' && message.status === 'deleted') {
+//           this.locations = this.locations.filter((loc) => loc !== message.location);
+//         }
+//       }
+//     });
+
+//   }
+
+//   ngOnInit(): void {
+//     this.http.get<{ locations: string[] }>('http://localhost:5000/api/locations').subscribe(
+//       (data) => {
+//         this.locations = data.locations;
+//         // 确保下拉框的初始值设为显示提示字
+//         this.form.get('goToLocation')?.setValue('');
+//         this.form.get('deleteLocation')?.setValue('');
+//       },
+//       (error) => {
+//         console.error('Failed to load locations:', error);
+//       }
+//     );
+//   }
+
+//   selectFunc(functionType: string) {
+//     this.selectedFunc = functionType;
+//   }
+
+//   sendCommand() {
+//     const command = this.selectedFunc;
+//     const args = this.form.get(this.selectedFunc)?.value || '';
+
+//     // 直接传递 args 对象
+//     this.webSocketService.sendCommand(command, args);
+
+//     // 重置表单字段
+//     this.form.get('addLocation')?.reset();
+//     this.form.get('goToLocation')?.setValue(''); // 确保提示字显示
+//     this.form.get('deleteLocation')?.setValue(''); // 确保提示字显示
+//     this.form.get('speak')?.reset();
+
+//     // 可选：重置选中的功能
+//     this.selectedFunc = '';
+//   }
+// }
+// //------------------------
 import { CommonModule, NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
@@ -11,43 +111,56 @@ import { WebSocketService } from '../websocket.service';
   templateUrl: './web-app-class.component.html',
   styleUrls: ['./web-app-class.component.css'],
 })
+// {
+// "_default":
+// {"4":
+//   {"id": "8bc4c48f-ffa2-439f-ac47-10f49f4a1d7e",
+//     "ip": "172.16.67.240",
+//     "location": "[home base, \u5165\u53e3, a]",
+//     "status": "Stand by"}}}
 export class WebAppClassComponent implements OnInit {
   form = new FormGroup({
+    ip: new FormControl('ip'),
     id: new FormControl(''),
-    ip: new FormControl(''),
     addLocation: new FormControl(''),
     goToLocation: new FormControl(''),
     deleteLocation: new FormControl(''),
     speak: new FormControl(''),
+    // selectedFunction: new FormControl(''),
+    // location: new FormControl(''),
   });
 
   selectedFunc: string = '';
-  locations: string[] = []; // 新增一个数组用于存储地点
-
+  locations: string[] = [];
+  messageid = "128";//this
+  messageip = "456"//this
   constructor(private webSocketService: WebSocketService, private http: HttpClient) {
     this.webSocketService.message$.subscribe((message) => {
-      console.log('Received message:', message);
+      console.log('Received message in WebAppClassComponent:', message);
       if (message) {
-        // 检查 message 是否为 null 或 undefined
+        if (message.type === 'onConnect') {//message.idmessage.ip
+          console.log('Updating form with ID and IP:', this.messageid, this.messageip);
+          this.form.patchValue({
+            id: this.messageid, //message.id,
+            ip: this.messageip,//message.ip,
+          });
+        }
         if (message.command === 'addLocation' && message.status === 'added') {
           this.locations.push(message.location);
         } else if (message.command === 'deleteLocation' && message.status === 'deleted') {
           this.locations = this.locations.filter((loc) => loc !== message.location);
-        } else if (message.type === 'onConnect') {
-          this.form.patchValue({
-            id: message.id,
-            ip: message.ip,
-          });
         }
+      } else {
+        console.warn('Received null or undefined message in WebAppClassComponent');
       }
     });
   }
+
 
   ngOnInit(): void {
     this.http.get<{ locations: string[] }>('http://localhost:5000/api/locations').subscribe(
       (data) => {
         this.locations = data.locations;
-        // 确保下拉框的初始值设为显示提示字
         this.form.get('goToLocation')?.setValue('');
         this.form.get('deleteLocation')?.setValue('');
       },
@@ -66,17 +179,13 @@ export class WebAppClassComponent implements OnInit {
     const args = this.form.get(this.selectedFunc)?.value || '';
 
     this.webSocketService.sendCommand(command, args);
-    // localStorage.setItem('lastCommand', JSON.stringify({ command, args }));
 
-    // 重置表单字段
-    // this.form.get('id')?.reset();
-    // this.form.get('ip')?.reset();
     this.form.get('addLocation')?.reset();
-    this.form.get('goToLocation')?.setValue(''); // 确保提示字显示
-    this.form.get('deleteLocation')?.setValue(''); // 确保提示字显示
+    this.form.get('goToLocation')?.setValue('');
+    this.form.get('deleteLocation')?.setValue('');
     this.form.get('speak')?.reset();
 
-    // 可选：重置选中的功能
     this.selectedFunc = '';
   }
 }
+
--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,3 +1,40 @@
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+// import io from 'socket.io-client';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class WebSocketService {
+//   private socket = io('http://localhost:5000'); // WebSocket URL
+//   public message$ = new BehaviorSubject<any>(null);
+
+//   constructor() {
+//     this.socket.on('connect', () => {
+//       console.log('WebSocket connected');
+//     });
+
+//     this.socket.on('disconnect', () => {
+//       console.log('WebSocket disconnected');
+//     });
+
+//     this.socket.on('error', (error) => {
+//       console.error('WebSocket error:', error);
+//     });
+
+//     // 监听 WebSocket 消息
+//     this.socket.on('response', (message: any) => {
+//       console.log('Received message:', message);
+//       this.message$.next(message);
+//     });
+//   }
+
+//   sendCommand(command: string, args: any) {
+//     console.log('Sending command:', { command, args });
+//     this.socket.emit('command', { command, args });
+//   }
+// }
+//------------------------------------
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import io from 'socket.io-client';
@@ -6,17 +43,42 @@ import io from 'socket.io-client';
   providedIn: 'root',
 })
 export class WebSocketService {
-  private socket = io('http://localhost:5000'); // 根据你的实际 WebSocket URL 修改
+  private socket = io('http://localhost:5000'); // WebSocket URL
   public message$ = new BehaviorSubject<any>(null);
 
   constructor() {
-    // 监听 WebSocket 消息
-    this.socket.on('response', (message: any) => {
-      this.message$.next(message);
+    this.socket.on('connect', () => {
+      console.log('WebSocket connected');
     });
+
+    this.socket.on('disconnect', () => {
+      console.log('WebSocket disconnected');
+    });
+
+    this.socket.on('error', (error: Error) => {
+      console.error('WebSocket error:', error);
+    });
+
+    // Listen for WebSocket messages
+    this.socket.on('response', (message: any) => {
+      console.log('Received message in WebSocketService:', message);
+      if (message) {
+        this.message$.next(message);
+      } else {
+        console.warn('Received null or undefined message');
+      }
+    });
+
+    // Optional: Test message emission
+    // You can use this code to simulate a message for testing purposes
+    setTimeout(() => {
+      this.message$.next({ type: 'onConnect', id: "None number", ip: "None number" });
+    }, 1000);
   }
 
-  sendCommand(command: string, args: string) {
+  sendCommand(command: string, args: any) {
+    console.log('Sending command:', { command, args });
     this.socket.emit('command', { command, args });
   }
 }
+
